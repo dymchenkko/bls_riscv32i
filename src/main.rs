e3f1538dd5abadfc65ad9e5eb5dffc992121b9e8
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2022-10-07 15:23:19 +0000

    verify hardcoded signature

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#![feature(default_alloc_error_handler)]
+#![no_main]
+#![no_std]
+pub extern crate externc_libm as libm;
+use core::{panic::PanicInfo};
+use signature_bls::{SecretKey, Signature, PublicKey};
+extern crate alloc;
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr::null_mut;
+use data_encoding::HEXLOWER;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern fn main(){
+    unsafe { 
+    let message = "080907000102040506070809020c0304080907000102040506070809020c0304";
+    let signat = "8613038c1684b048b63328b35d31fe14a144010316fe25bde58a268674ab3ae5bfdb910c9fa7c347e4c3d4063d305c8c";
+    let pk = "a2359fa8a69cc2bc8c71017402fc9faf43159354173c72822c0b4685c2c0acd4cfb06349dc883c7c23150f93eeb390780b486303eea37af882f62cb3ab5cecd03dffbbb8a11f99950007764da9c730d2e1604b4f7e4a1c570aa88f84e3bd22c1";
+
+    let result = Signature::from_bytes(&HEXLOWER.decode(&signat.as_bytes()).unwrap().try_into().unwrap()).unwrap().verify(PublicKey::from_bytes(&HEXLOWER.decode(&pk.as_bytes()).unwrap().try_into().unwrap()).unwrap(), &HEXLOWER.decode(message.as_bytes()).unwrap());
+    assert_eq!(result.unwrap_u8(), 1);    
+    }
+}
+
+
+pub struct Allocator;
+
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        panic!("dealloc should be never called")
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: Allocator = Allocator;
